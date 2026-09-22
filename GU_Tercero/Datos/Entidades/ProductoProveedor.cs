8457cdc5956c9a6c8ca8acf73e35a3ee65d48f27
Ramil-Micaias <micram2006@gmail.com>
2026-09-22 18:00:23 -0300

    Entidades nuevas

---
+++ GU_Tercero/Datos/Entidades/ProductoProveedor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Entidades
+{
+    public class ProductoProveedor
+    {
+        public int Id_ProductoProveedor { get; set; }
+        public int Id_Producto { get; set; }
+        public int Id_Proveedor { get; set; }
+        public bool Activo { get; set; }
+    }
+}
