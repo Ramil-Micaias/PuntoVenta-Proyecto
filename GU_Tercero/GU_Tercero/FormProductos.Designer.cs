930f904612e226e77b482c8d2e23c361641af856
Estudiante <estudiante@terciario.edu.ar>
2026-09-22 00:26:59 -0300

    Solución definitiva: TAB enfocado unicamente en textboxes y combo, usuario alineado dinamicamente

--- GU_Tercero/GU_Tercero/FormProductos.Designer.cs
+++ GU_Tercero/GU_Tercero/FormProductos.Designer.cs
@@ -65,13 +65,7 @@
             lblNombreCategoria = new Label();
             lblListadoCategoria = new Label();
             dataGridView1 = new DataGridView();
-            tabProductoCategoria.Controls.Add(tabPage1);
-            tabProductoCategoria.Controls.Add(tabPage2);
-            tabProductoCategoria.Location = new Point(0, 0);
-            tabProductoCategoria.Name = "tabProductoCategoria";
-            tabProductoCategoria.SelectedIndex = 0;
-            tabProductoCategoria.Size = new Size(801, 450);
-            tabProductoCategoria.TabIndex = 0;
+            tabProductoCategoria.SuspendLayout();
             tabPage1.SuspendLayout();
             panelProductos.SuspendLayout();
             panel1.SuspendLayout();
@@ -157,32 +151,7 @@
             lblBarra.Name = "lblBarra";
             lblBarra.Size = new Size(92, 15);
             lblBarra.TabIndex = 19;
-            lblBarra.TabStop = false;
             lblBarra.Text = "Codigo de Barra";
-            // 
-            // txtBarra
-            // 
-            txtBarra.Location = new Point(302, 5);
-            txtBarra.Name = "txtBarra";
-            txtBarra.Size = new Size(100, 23);
-            txtBarra.TabIndex = 6;
-            // 
-            // txtStockMinimo
-            // 
-            txtStockMinimo.Location = new Point(85, 142);
-            txtStockMinimo.Name = "txtStockMinimo";
-            txtStockMinimo.Size = new Size(100, 23);
-            txtStockMinimo.TabIndex = 5;
-            // 
-            // lblStockMinimo
-            // 
-            lblStockMinimo.AutoSize = true;
-            lblStockMinimo.Location = new Point(1, 145);
-            lblStockMinimo.Name = "lblStockMinimo";
-            lblStockMinimo.Size = new Size(81, 15);
-            lblStockMinimo.TabIndex = 16;
-            lblStockMinimo.TabStop = false;
-            lblStockMinimo.Text = "Stock Minimo";
             // 
             // txtNombreProducto
             // 
@@ -191,73 +160,47 @@
             txtNombreProducto.Size = new Size(100, 23);
             txtNombreProducto.TabIndex = 0;
             // 
-            // lblNombreProducto
+            // txtDescripcion
             // 
-            lblNombreProducto.AutoSize = true;
-            lblNombreProducto.Location = new Point(28, 7);
-            lblNombreProducto.Name = "lblNombreProducto";
-            lblNombreProducto.Size = new Size(51, 15);
-            lblNombreProducto.TabIndex = 14;
-            lblNombreProducto.TabStop = false;
-            lblNombreProducto.Text = "Nombre";
+            txtDescripcion.Location = new Point(85, 31);
+            txtDescripcion.Name = "txtDescripcion";
+            txtDescripcion.Size = new Size(100, 23);
+            txtDescripcion.TabIndex = 1;
             // 
-            // btnModificar
+            // txtPrecioCosto
             // 
-            btnModificar.BackColor = Color.FromArgb(37, 99, 235);
-            btnModificar.FlatAppearance.BorderSize = 0;
-            btnModificar.FlatStyle = FlatStyle.Flat;
-            btnModificar.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
-            btnModificar.ForeColor = Color.White;
-            btnModificar.Location = new Point(698, 30);
-            btnModificar.Name = "btnModificar";
-            btnModificar.Size = new Size(75, 23);
-            btnModificar.TabIndex = 9;
-            btnModificar.TabStop = false;
-            btnModificar.Text = "Modificar";
-            btnModificar.UseVisualStyleBackColor = false;
-            btnModificar.Click += btnModificar_Click;
+            txtPrecioCosto.Location = new Point(85, 59);
+            txtPrecioCosto.Name = "txtPrecioCosto";
+            txtPrecioCosto.Size = new Size(100, 23);
+            txtPrecioCosto.TabIndex = 2;
             // 
-            // btnGuardar
+            // txtPrecioVenta
             // 
-            btnGuardar.BackColor = Color.FromArgb(22, 163, 74);
-            btnGuardar.FlatAppearance.BorderSize = 0;
-            btnGuardar.FlatStyle = FlatStyle.Flat;
-            btnGuardar.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
-            btnGuardar.ForeColor = Color.White;
-            btnGuardar.Location = new Point(698, 58);
-            btnGuardar.Name = "btnGuardar";
-            btnGuardar.Size = new Size(75, 23);
-            btnGuardar.TabIndex = 10;
-            btnGuardar.TabStop = false;
-            btnGuardar.Text = "Guardar";
-            btnGuardar.UseVisualStyleBackColor = false;
-            btnGuardar.Click += btnGuardar_Click;
+            txtPrecioVenta.Location = new Point(85, 87);
+            txtPrecioVenta.Name = "txtPrecioVenta";
+            txtPrecioVenta.Size = new Size(100, 23);
+            txtPrecioVenta.TabIndex = 3;
             // 
-            // btnNuevo
+            // txtStockActual
             // 
-            btnNuevo.BackColor = Color.FromArgb(71, 85, 105);
-            btnNuevo.FlatAppearance.BorderSize = 0;
-            btnNuevo.FlatStyle = FlatStyle.Flat;
-            btnNuevo.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold);
-            btnNuevo.ForeColor = Color.White;
-            btnNuevo.Location = new Point(698, 4);
-            btnNuevo.Name = "btnNuevo";
-            btnNuevo.Size = new Size(75, 23);
-            btnNuevo.TabIndex = 8;
-            btnNuevo.TabStop = false;
-            btnNuevo.Text = "Nuevo";
-            btnNuevo.UseVisualStyleBackColor = false;
-            btnNuevo.Click += btnNuevo_Click;
+            txtStockActual.Location = new Point(85, 115);
+            txtStockActual.Name = "txtStockActual";
+            txtStockActual.Size = new Size(100, 23);
+            txtStockActual.TabIndex = 4;
             // 
-            // lblCategorias
+            // txtStockMinimo
             // 
-            lblCategorias.AutoSize = true;
-            lblCategorias.Location = new Point(439, 11);
-            lblCategorias.Name = "lblCategorias";
-            lblCategorias.Size = new Size(63, 15);
-            lblCategorias.TabIndex = 9;
-            lblCategorias.TabStop = false;
-            lblCategorias.Text = "Categorias";
+            txtStockMinimo.Location = new Point(85, 142);
+            txtStockMinimo.Name = "txtStockMinimo";
+            txtStockMinimo.Size = new Size(100, 23);
+            txtStockMinimo.TabIndex = 5;
+            // 
+            // txtBarra
+            // 
+            txtBarra.Location = new Point(302, 5);
+            txtBarra.Name = "txtBarra";
+            txtBarra.Size = new Size(100, 23);
+            txtBarra.TabIndex = 6;
             // 
             // cmbCategoria
             // 
@@ -267,63 +210,38 @@
             cmbCategoria.Size = new Size(179, 23);
             cmbCategoria.TabIndex = 7;
             // 
-            // txtStockActual
+            // btnNuevo
             // 
-            txtStockActual.Location = new Point(85, 115);
-            txtStockActual.Name = "txtStockActual";
-            txtStockActual.Size = new Size(100, 23);
-            txtStockActual.TabIndex = 4;
+            btnNuevo.Location = new Point(698, 4);
+            btnNuevo.Name = "btnNuevo";
+            btnNuevo.Size = new Size(75, 23);
+            btnNuevo.TabIndex = 8;
+            btnNuevo.TabStop = false;
+            btnNuevo.Text = "Nuevo";
+            btnNuevo.UseVisualStyleBackColor = true;
+            btnNuevo.Click += btnNuevo_Click;
             // 
-            // lblStockActual
+            // btnModificar
             // 
-            lblStockActual.AutoSize = true;
-            lblStockActual.Location = new Point(10, 119);
-            lblStockActual.Name = "lblStockActual";
-            lblStockActual.Size = new Size(73, 15);
-            lblStockActual.TabIndex = 6;
-            lblStockActual.TabStop = false;
-            lblStockActual.Text = "Stock Actual";
+            btnModificar.Location = new Point(698, 30);
+            btnModificar.Name = "btnModificar";
+            btnModificar.Size = new Size(75, 23);
+            btnModificar.TabIndex = 9;
+            btnModificar.TabStop = false;
+            btnModificar.Text = "Modificar";
+            btnModificar.UseVisualStyleBackColor = true;
+            btnModificar.Click += btnModificar_Click;
             // 
-            // txtPrecioVenta
+            // btnGuardar
             // 
-            txtPrecioVenta.Location = new Point(85, 87);
-            txtPrecioVenta.Name = "txtPrecioVenta";
-            txtPrecioVenta.Size = new Size(100, 23);
-            txtPrecioVenta.TabIndex = 3;
-            // 
-            // lblPrecioVenta
-            // 
-            lblPrecioVenta.AutoSize = true;
-            lblPrecioVenta.Location = new Point(11, 91);
-            lblPrecioVenta.Name = "lblPrecioVenta";
-            lblPrecioVenta.Size = new Size(72, 15);
-            lblPrecioVenta.TabIndex = 4;
-            lblPrecioVenta.TabStop = false;
-            lblPrecioVenta.Text = "Precio Venta";
-            // 
-            // txtPrecioCosto
-            // 
-            txtPrecioCosto.Location = new Point(85, 59);
-            txtPrecioCosto.Name = "txtPrecioCosto";
-            txtPrecioCosto.Size = new Size(100, 23);
-            txtPrecioCosto.TabIndex = 2;
-            // 
-            // lblPrecioCosto
-            // 
-            lblPrecioCosto.AutoSize = true;
-            lblPrecioCosto.Location = new Point(11, 62);
-            lblPrecioCosto.Name = "lblPrecioCosto";
-            lblPrecioCosto.Size = new Size(74, 15);
-            lblPrecioCosto.TabIndex = 2;
-            lblPrecioCosto.TabStop = false;
-            lblPrecioCosto.Text = "Precio Costo";
-            // 
-            // txtDescripcion
-            // 
-            txtDescripcion.Location = new Point(85, 31);
-            txtDescripcion.Name = "txtDescripcion";
-            txtDescripcion.Size = new Size(100, 23);
-            txtDescripcion.TabIndex = 1;
+            btnGuardar.Location = new Point(698, 58);
+            btnGuardar.Name = "btnGuardar";
+            btnGuardar.Size = new Size(75, 23);
+            btnGuardar.TabIndex = 10;
+            btnGuardar.TabStop = false;
+            btnGuardar.Text = "Guardar";
+            btnGuardar.UseVisualStyleBackColor = true;
+            btnGuardar.Click += btnGuardar_Click;
             // 
             // lblDescripcion
             // 
@@ -332,7 +250,6 @@
             lblDescripcion.Name = "lblDescripcion";
             lblDescripcion.Size = new Size(69, 15);
             lblDescripcion.TabIndex = 0;
-            lblDescripcion.TabStop = false;
             lblDescripcion.Text = "Descripción";
             // 
             // dgvProductos
